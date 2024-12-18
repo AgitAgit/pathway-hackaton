0c04bd48267693e8034b1ba155e8b70d0554d790
Amit Kubani <amitkubani@gmail.com>
2024-12-18 23:52:23 +0200

    commit

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -22,6 +22,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
+      enum: ["user", "mentor", "developer", "expert"],
       required: false,
       default: 'user'
     },
