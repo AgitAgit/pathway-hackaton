b07f2d882b3297be1902e5c6af33593e9089296d
Itay Benadon <itay945@yahoo.com>
2024-12-18 10:41:56 +0200

    create NavBar Components

--- client/src/Pages/Article/Article.tsx
+++ client/src/Pages/Article/Article.tsx
@@ -1,15 +1,17 @@
 import styles from "./Article.module.css";
 import { Outlet } from "react-router-dom";
+import NavBar from "../../Components/NavBar/NavBar";
 
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      I AM ATRICLE
-      <Outlet />
-    </div>
-  );
+   return (
+      <div>
+         I AM ATRICLE
+         <NavBar />
+         <Outlet />
+      </div>
+   );
 }
 
 export default Article;
