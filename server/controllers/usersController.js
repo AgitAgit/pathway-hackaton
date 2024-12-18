ab6c42f6f8c1fe841bd0d456f4382881c03bcd4c
Amit Kubani <amitkubani@gmail.com>
2024-12-18 21:59:56 +0200

    commit

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -47,30 +47,30 @@ async function login(req, res, next) {
     if (!username || !password)
       return res
     .status(400)
-    .json({ message: "username and password are required..." });
+    .json({ message: "username and password are required...", login:false});
     const storedUser = await User.findOne({ username: username }); //check if the user exists and extract it from the db
     // console.log("stored user:", storedUser);
     if (!storedUser)
       return res
         .status(400)
-        .json({ message: `could not find user ${username}` });
+        .json({ message: `could not find user ${username}`, login:false});
     const isValid = bcrypt.compareSync(password, storedUser.password); //use bcrypt to test if the login password matches the stored one
     if (!isValid)
-      return res.status(400).json({ message: "Invalid password..." });
-    const token = jwt.sign(
-      //generate a jwt token with payload containing the username, userId, and user role.
-      {
-        user: {
-          username,
-          userId: storedUser._id,
-          role: storedUser.role || "user",
-        },
-      },
-      secretKey,
-      { expiresIn: "1h" }
-    );
+      return res.status(400).json({ message: "Invalid password...", login:false });
+    // const token = jwt.sign(
+    //   //generate a jwt token with payload containing the username, userId, and user role.
+    //   {
+    //     user: {
+    //       username,
+    //       userId: storedUser._id,
+    //       role: storedUser.role || "user",
+    //     },
+    //   },
+    //   secretKey,
+    //   { expiresIn: "1h" }
+    // );
     console.log("a user has logged in...");
-    res.status(200).json({ message: `User ${username} logged in successfully.`, token });
+    res.status(200).json({ message: `User ${username} logged in successfully.`, login:true });
     } catch (error) {
     next(error);
   }
