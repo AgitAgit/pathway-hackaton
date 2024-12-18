9561676dc3f6e5aa5dbe40a072699e1b4370543b
Amit Kubani <amitkubani@gmail.com>
2024-12-18 18:00:52 +0200

    commit

--- server/controllers/professionsController.js
+++ server/controllers/professionsController.js
@@ -1,7 +1,7 @@
 const Profession = require('../models/professionModel.js');
 const Training = require('../models/trainingModel.js');
 
-module.exports = { addProfessions, getProfessions, addTrainingToProfessionByNames }
+module.exports = { addProfessions, getProfessions, getProfessionsMinimal, addTrainingToProfessionByNames }
 //add profession
 async function addProfessions(req, res, next) {
     try {
@@ -23,6 +23,17 @@ async function getProfessions(req, res, next) {
         next(error);
     }
 }
+
+async function getProfessionsMinimal(req, res, next) {
+    try {
+        const professions = await Profession.find({},{name:1, _id:1});
+        res.json(professions);
+    } catch (error) {
+        next(error);
+    }
+}
+
+
 //get by id
 //update profession
 async function addTrainingToProfessionByNames(req, res, next) {
@@ -30,6 +41,13 @@ async function addTrainingToProfessionByNames(req, res, next) {
         const { professionName, trainingName } = req.body;
         const profession = await Profession.findOne({name:professionName});
         const training = await Training.findOne({name:trainingName});
+        if(profession.requiredTrainingIds.includes(training._id)){
+            res.json({message:"can not add duplicates..."});
+        } 
+        else {
+            profession.requiredTrainingIds.push(training._id);
+        }
+        await profession.save();
         res.json({ pro:profession, tra:training});
     } catch (error) {
         next(error);
