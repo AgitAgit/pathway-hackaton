a9c3587105194b5d3b8afc7a6ac2d0a3a66bd322
Amit Kubani <amitkubani@gmail.com>
2024-12-18 21:31:28 +0200

    added addTraining

--- server/controllers/trainingControlller.js
+++ server/controllers/trainingControlller.js
@@ -2,6 +2,16 @@ const Training = require('../models/trainingModel.js');
 
 module.exports = { getAllTraining, getAllTrainingMinimal };
 
+async function addTraining(req, res, next){
+    try{
+        const { training } = req.body;
+        const result = await Training.insertOne(training);
+        res.json(result);
+    } catch (error) {
+        next(error)
+    }
+}
+
 async function getAllTraining(req, res, next){
     try{
         const training = await Training.find();
