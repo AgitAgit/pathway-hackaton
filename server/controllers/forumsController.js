30af768975eb29fcdcf84deac182b15547557cda
Amit Kubani <amitkubani@gmail.com>
2024-12-19 07:09:31 +0200

    populate forums!

--- server/controllers/forumsController.js
+++ server/controllers/forumsController.js
@@ -2,8 +2,8 @@ const Forum = require("../models/forumModel.js");
 
 module.exports = { addForums, getAllForums }
 
-async function addForums(req, res, next){
-    try{
+async function addForums(req, res, next) {
+    try {
         const { forums } = req.body;
         const result = await Forum.insertMany(forums);
         res.json(result);
@@ -12,9 +12,24 @@ async function addForums(req, res, next){
     }
 }
 
-async function getAllForums(req, res, next){
+async function getAllForums(req, res, next) {
     try {
-        const forums = await Forum.find().populate("postIds");
+        const forums = await Forum.find()
+            .populate("postIds")
+            .populate({
+                path: "postIds",
+                populate: { path: "authorId", select: "username role" },
+            })
+            .populate({
+                path: "postIds",
+                populate: { path: "commentIds" },
+            })
+            .populate({
+                path: "postIds",
+                populate: { 
+                    path: "commentIds", 
+                    populate: { path: "authorId", select: "username role" } },
+            })
         res.json(forums);
     } catch (error) {
         next(error);
