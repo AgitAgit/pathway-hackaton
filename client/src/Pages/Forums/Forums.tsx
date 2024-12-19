6cac7a93bd53810f41113f8a9896baf2cb7dc10b
Amit Kubani <amitkubani@gmail.com>
2024-12-19 07:32:39 +0200

    commit

--- client/src/Pages/Forums/Forums.tsx
+++ client/src/Pages/Forums/Forums.tsx
@@ -1,18 +1,36 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 //get logged in user data
 //fetch forums function
 //create forum function
+// const response = await axios.post(
+//     "http://localhost:3000/api/users/login",
+//     {
+//       username,
+//       password,
+//     }
+//   );
 //create post function
 //create comment function
 
-function Forms({}){
+function Forums({}){
     const user = useSelector((state) => state.user.user);
     console.log(user);
+
+    async function getForumsData(){
+        try {
+            const response = await axios.get(
+                "http://localhost:3000/api/forums");
+        } catch (error) {
+            
+        }
+    }
+
     return<>
 
     </>
 }
 
-export default Forms;
+export default Forums;
